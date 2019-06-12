35b0d08d2cd331084ba5b97d3ed323ce4a4b70f5
Jérémy Morel <jeremy.morel@owkin.com>
2019-06-12 15:07:58 +0200

    codeSample: fix test config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
     snapshotSerializers: ['jest-emotion'],
+    moduleNameMapper: {
+        'react-syntax-highlighter/dist/esm/styles/prism': '<rootDir>/__mocks__/prismMock.js'
+    },
 };
