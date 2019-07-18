c2f73b13c985ce8db38f023f265178078b92648d
GuillaumeCisco <guillaumecisco@gmail.com>
2019-07-18 11:47:48 +0200

    clean jest config

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,8 @@ module.exports = {
            Taken from https://jestjs.io/docs/en/webpack#handling-static-assets
            Needed for handling the font files
          */
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/mocks/fileMock.js",
-        "\\.(css|less)$": "<rootDir>/test/mocks/styleMock.js"
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/mocks/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/test/mocks/styleMock.js'
     },
     setupFilesAfterEnv: [
         '@testing-library/react/cleanup-after-each',
